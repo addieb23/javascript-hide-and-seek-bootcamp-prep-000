52f0bd1e50d1592cac951dce3df2ef53a1a7dae1
Addie Brunk <mlbrunk23@gmail.com>
2018-06-04 19:34:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  let first = document.querySelector(selector);
+  return first;
+}
+
+function nestedTarget(){
+  let targ = document.querySelector('div');
+  return targ;
+}
+
+function increaseRankBy(n){
+  let rank = document.querySelector
+  return rank;
+}
+
+
+// function deepestChild(){
+//    let len = document.getElementById('grand-node').childNodes.length;
+//    return document.getElementById('grand-node').childNodes[len-1];
+// }
